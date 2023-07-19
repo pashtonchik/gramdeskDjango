5129387ddf38994a8892b33769502dedeed88bba
pashtonchik <pashka191@yandex.ru>
2023-07-19 03:15:51 +0300

    asdasda

--- tickets/settings.py
+++ tickets/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@4sam!l!rt52jm_45govd0+rws#ieetc-0!fa$sy&&d5c8no_o
 DEBUG = True
 CHANNEL_REDIS_HOST = 6380
 ALLOWED_HOSTS = ['*']
-
+AUTH_USER_MODEL = 'dispatcher.User'
 
 
 # Application definition
