5f029741c0d8c93f2477cc5d0a95d1dbf7782c5a
pashtonchik <pashka191@yandex.ru>
2023-09-25 21:56:01 +0300

    asdasdasda

--- tickets/background/celery.py
+++ tickets/background/celery.py
@@ -1,4 +1,5 @@
 from celery import Celery
+
 from tickets.settings import APP_NAME_CELERY
 import os
 
@@ -7,3 +8,14 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tickets.settings')
 app = Celery(APP_NAME_CELERY)
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(10.0, update_items_fee.s())
+
+
+@app.task
+def update_items_fee():
+
+    print('сменили комсу всем пользователям')
