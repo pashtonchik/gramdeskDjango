ae7dda0107d47fda91d01165b4db6542a0b2f724
pashtonchik <pashka191@yandex.ru>
2023-09-25 20:14:58 +0300

    asdasdad

--- tickets/settings.py
+++ tickets/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'backend.socket_heartbeat.HeartbeatMiddleware'
 ]
 
 CELERY_BROKER_URL = PORTREDIS
