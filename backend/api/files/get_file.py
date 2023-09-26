d574f9cd77ddc0e323ffbb64229aa9baecf36c4f
pashtonchik <pashka191@yandex.ru>
2023-09-26 17:55:12 +0300

    asdasda

--- backend/api/files/get_file.py
+++ backend/api/files/get_file.py
@@ -29,7 +29,7 @@ def get_attachment(request, attachment):
     ext = os.path.splitext(str(cur_attachment.file.filename))[1]
     # sending response
     response = HttpResponse(file_data, content_type=f'application/{ext}')
-    response['Content-Disposition'] = f'attachment; filename="{cur_attachment.file.filename}"'
+    response['Content-Disposition'] = f'attachment; filename="{cur_attachment.file.name}"'
 
     # return FileResponse(open(file_location, 'rb'))
 
