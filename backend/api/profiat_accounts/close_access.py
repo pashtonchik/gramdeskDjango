d3883a1b95c44d671bae83b44c5269e58b3b522b
pashtonchik <pashka191@yandex.ru>
2023-09-27 23:59:08 +0300

    asdasdada

---
+++ backend/api/profiat_accounts/close_access.py
@@ -0,0 +1,51 @@
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+from rest_framework import status
+from rest_framework.response import Response
+import json
+from django.db import transaction
+import logging
+from backend.models import JWTToken, SocketConnection, User
+from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken
+from rest_framework.decorators import api_view, permission_classes
+
+
+@api_view(["POST"])
+@transaction.atomic()
+def close_access(request):
+    data = json.loads(request.body.decode("utf-8"))
+    logger = logging.getLogger("mylogger")
+    logger.info(data)
+
+    try:
+        profiat_id = data['id']
+    except KeyError:
+        return Response(status=status.HTTP_400_BAD_REQUEST, data={'ok': False, 'message': "Field Refresh is required."})
+
+
+    current_user = User.objects.filter(profiat_id=profiat_id)
+    if current_user.count != 1:
+        return Response(status=status.HTTP_400_BAD_REQUEST, data={'ok': False, 'message': "User does not exists."})
+    else:
+        current_user = current_user.first()
+
+    sockets_for_close = SocketConnection.objects.select_for_update().filter(user=current_user, active=True)
+    jwt_to_close = JWTToken.objects.select_for_update().filter(user=current_user, active=True)
+
+    jwt_to_close.update(active=False)
+
+    token_mas = []
+    for i in jwt_to_close:
+        token_mas.append(BlacklistedToken(token=i.refresh))
+    BlacklistedToken.objects.bulk_create(token_mas, ignore_conflicts=True)
+
+    channel_layer = get_channel_layer()
+    for connection in sockets_for_close:
+        async_to_sync(channel_layer.group_add)(f'close_access', connection.channel_name)
+
+    async_to_sync(channel_layer.group_send)("close_access", {"type": "disconnect.by.heartbeat",
+                                                                   "message": "disconnect"})
+
+    sockets_for_close.update(active=False)
+
+    return Response(status=status.HTTP_200_OK, data={"ok": True})
--- backend/urls.py
+++ backend/urls.py
@@ -5,6 +5,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from backend.api.files.get_file import get_attachment
 from backend.api.message_from_telegram.message_from_user import new_message
 from backend.api.profiat_accounts.auth import profiat_auth_client
+from backend.api.profiat_accounts.close_access import close_access
 from backend.api.profiat_accounts.refresh import refresh
 from backend.permissions import ProfiatIntegration
 
@@ -13,16 +14,7 @@ router = routers.DefaultRouter()
 urlpatterns = [
     path('api/v2/client/auth/', profiat_auth_client),
     path('api/v2/client/auth/refresh/', refresh),
-    # path('api/v3/accounts/auth/', auth),
-    # path('api/v3/accounts/auth/refresh/', TokenRefreshView.as_view()),
-    # path('api/v3/accounts/signup/', registrate),
-    # path('api/v3/accounts/signup/action/', continue_registration),
-    # path('api/v3/accounts/edit/password/', edit_password),
-    # path('api/v3/accounts/restore/', restore),
-    # path('api/v3/accounts/wallet/connect/', connect_flex),
-    # path('api/v3/accounts/info/', get_account_info),
-    # path('api/v3/accounts/dashboard/', get_account_info),
-    # path('api/v3/accounts/edit/profile/', edit_profile_data),
+    path('api/v2/client/auth/close/access/', close_access),
 
     # Enable 2FA
     path('api/v1/new/message/', new_message),
