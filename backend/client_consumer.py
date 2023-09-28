8b3be32846f946eca1b470e4c044830e8730f42c
pashtonchik <pashka191@yandex.ru>
2023-09-28 19:28:46 +0300

    asdasdasda

--- backend/client_consumer.py
+++ backend/client_consumer.py
@@ -121,6 +121,11 @@ class ClientConsumer(WebsocketConsumer):
             message_text=new_message['content'],
             ticket=ticket,
         )
+
+        if 'message_to_reply' in data:
+            if data['message_to_reply']:
+                message.message_to_reply = TicketMessage.objects.get(id=data['message_to_reply']['id'], ticket=message.ticket)
+
         message.save()
 
         responce_data = {
