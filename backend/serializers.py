d37894df47a9f33b038c25884d9e101584bbb6d3
pashtonchik <pashka191@yandex.ru>
2023-09-28 17:36:17 +0300

    asdasdasda

--- backend/serializers.py
+++ backend/serializers.py
@@ -5,17 +5,18 @@ from backend.models import Ticket, TicketMessage
 class TicketMessageSerializer(serializers.ModelSerializer):
     chat_id = serializers.CharField(source='get_ticket_id', read_only=True)
     sender_id = serializers.CharField(source='get_sender_id', read_only=True)
-    is_outgoing = serializers.ReadOnlyField(source=serializers.SerializerMethodField('get_is_outgoing'))
+    is_outgoing = serializers.SerializerMethodField('get_is_outgoing')
     content = serializers.ReadOnlyField(source='message_text')
     media = serializers.ReadOnlyField(source='get_file')
     date = serializers.ReadOnlyField(source='get_date')
 
+
     def get_is_outgoing(self, obj):
         user_type = self.context.get("from_user_type")
         if user_type == 'support':
-            return 'get_is_outgoing_support'
+            return serializers.ReadOnlyField(source='get_is_outgoing_support')
         else:
-            return 'get_is_outgoing_client'
+            return serializers.ReadOnlyField(source='get_is_outgoing_client')
 
     class Meta:
         fields = ('id', 'chat_id', 'sending_state', 'sender_id', 'is_outgoing', 'content', 'media', 'date')
