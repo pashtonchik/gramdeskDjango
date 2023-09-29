41c2de39c676f1c52137deaba3fb1cf4b4cf8bee
pashtonchik <pashka191@yandex.ru>
2023-09-29 21:01:27 +0300

    asdasdad

--- tickets/settings.py
+++ tickets/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'backend',
+    'corsheaders',
     'rest_framework_simplejwt.token_blacklist',
     # 'rest_framework',
     'django.contrib.staticfiles',
