6e97f4c21b54ffce8df6e21778cb0b46ee6e9837
pashtonchik <pashka191@yandex.ru>
2023-09-29 17:10:59 +0300

    asdasdasda

--- tickets/asgi.py
+++ tickets/asgi.py
@@ -1,35 +1,41 @@
+# import os
+#
+# import django
+# from channels.auth import AuthMiddlewareStack
+# from channels.security.websocket import AllowedHostsOriginValidator
+# from django.conf import settings
+#
+# from django.core.asgi import get_asgi_application
+# from channels.routing import ProtocolTypeRouter, URLRouter, ChannelNameRouter
+# from django.urls import path, re_path
+#
+# from backend.consumer import LiveScoreConsumer
+# from backend.client_consumer import ClientConsumer
+# from backend.socket_auth import TokenAuthMiddleware
+# from backend.socket_heartbeat import HeartbeatMiddleware
+# from tickets.wsgi import *
+# # from .wsgi import application
+# settings.configure()
 import os
 
-import django
-from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator
-from django.conf import settings
+from channels.routing import ProtocolTypeRouter
 
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter, ChannelNameRouter
-from django.urls import path, re_path
-
-from backend.consumer import LiveScoreConsumer
-from backend.client_consumer import ClientConsumer
-from backend.socket_auth import TokenAuthMiddleware
-from backend.socket_heartbeat import HeartbeatMiddleware
-from tickets.wsgi import *
-# from .wsgi import application
-settings.configure()
 DJANGO_SETTINGS_MODULE = os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tickets.settings')
 
 
 # application = get_asgi_application()
 
 
-application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-    'websocket': AllowedHostsOriginValidator(HeartbeatMiddleware(TokenAuthMiddleware(
-        URLRouter(
-            [
-                re_path("apiapi/", LiveScoreConsumer.as_asgi()),
-                re_path("client/", ClientConsumer.as_asgi()),
-            ]
-        )
-    )))
-})
+application = ProtocolTypeRouter(
+#     {
+#     'http': get_asgi_application(),
+#     'websocket': AllowedHostsOriginValidator(HeartbeatMiddleware(TokenAuthMiddleware(
+#         URLRouter(
+#             [
+#                 re_path("apiapi/", LiveScoreConsumer.as_asgi()),
+#                 re_path("client/", ClientConsumer.as_asgi()),
+#             ]
+#         )
+#     )))
+# }
+)
