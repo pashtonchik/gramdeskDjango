ebf1748d0e6c4fc51b9357e9c1b18ec9a5160385
pashtonchik <pashka191@yandex.ru>
2023-09-30 01:08:43 +0300

    asdasdad

--- tickets/settings.py
+++ tickets/settings.py
@@ -213,7 +213,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://pashtonp.space']
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # if DEBUG:
