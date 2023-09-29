bd7e0f5bdd83bf7bdb70ebdd03d3952f4f79dcae
pashtonchik <pashka191@yandex.ru>
2023-09-29 20:55:56 +0300

    asdasdad

--- tickets/settings.py
+++ tickets/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-@4sam!l!rt52jm_45govd0+rws#ieetc-0!fa$sy&&d5c8no_o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 CHANNEL_REDIS_HOST = 6380
-ALLOWED_HOSTS = ['https://pashtonp.space', 'http://185.138.164.171']
+ALLOWED_HOSTS = ['https://pashtonp.space', '185.138.164.171']
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # if DEBUG:
@@ -41,7 +41,7 @@ CORS_ALLOW_ALL_ORIGINS=True
 CORS_ALLOW_HEADERS = [
     "*"
 ]
-CSRF_TRUSTED_ORIGINS = ['https://pashtonp.space', 'http://185.138.164.171']
+CSRF_TRUSTED_ORIGINS = ['https://pashtonp.space', '185.138.164.171']
 # if not DEBUG:
 #     CSRF_TRUSTED_ORIGINS = ['https://pashtonp.space'] # FIX admin CSRF token issue
 
