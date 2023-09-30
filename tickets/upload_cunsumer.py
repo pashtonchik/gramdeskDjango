2db143e97a443012632b5bcc8dd95cdfb8b0d127
pashtonchik <pashka191@yandex.ru>
2023-09-30 17:22:50 +0300

    asdasdad

--- tickets/upload_cunsumer.py
+++ tickets/upload_cunsumer.py
@@ -5,7 +5,10 @@ from json import JSONDecodeError
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 from channels.layers import get_channel_layer
+from django.core.files.base import ContentFile
 from django.db import transaction
+
+from backend.serializers import AttachmentSerializer
 
 
 class UploadConsumer(WebsocketConsumer):
@@ -42,14 +45,57 @@ class UploadConsumer(WebsocketConsumer):
         from backend.models import Ticket, TicketMessage, User, Attachment
         from backend.serializers import TicketSerializer, TicketMessageSerializer
         upload_data = data['upload_data']
+
+        current_attachment = Attachment.objects.select_for_update().get(id=upload_data['id'])
         # current_attachment = Attachment.objects.select_for_update().get(id=upload_data['id'], uploaded=False)
-
         received_bytes = upload_data['content']
-        print(len(base64.b64decode(received_bytes.encode('UTF-8'))))
+        content = base64.b64decode(received_bytes.encode('UTF-8'))
+        current_attachment.received_bytes += len(base64.b64decode(received_bytes.encode('UTF-8')))
+        current_attachment.content += received_bytes
         print('file')
-        with open(f'123.pdf', 'ab+') as file:
-            file.write(base64.b64decode(received_bytes.encode('UTF-8')))
+        # with open(f'123.pdf', 'ab+') as file:
+        #     file.write(base64.b64decode(received_bytes.encode('UTF-8')))
 
+        if current_attachment.total_bytes == current_attachment.received_bytes:
+            current_attachment.file.save(name=current_attachment.name + '.' + current_attachment.ext,
+                                         content=ContentFile(base64.b64decode(current_attachment.content)),
+                                         save=True
+                                         )
+            current_attachment.uploaded = True
+
+            current_attachment.save()
+
+            if not Attachment.objects.filter(message=current_attachment.message, uploaded=False).exists():
+                current_message = TicketMessage.objects.select_for_update().get(id=current_attachment.id)
+                current_message.sending_state = 'sent'
+
+                output_data_clients = {
+                    'event': "incoming",
+                    'type': 'new_message',
+                    'message': TicketMessageSerializer(current_message, context={"from_user_type": "client"}).data,
+                }
+
+                output_data_supports = {
+                    'event': "incoming",
+                    'type': 'new_message',
+                    'message': TicketMessageSerializer(current_message, context={"from_user_type": "support"}).data,
+                }
+
+                channel_layer = get_channel_layer()
+                async_to_sync(channel_layer.group_send)("active_support", {"type": "chat.message",
+                                                                           "message": json.dumps(output_data_supports)})
+                async_to_sync(channel_layer.group_send)(f"client_{current_message.tg_user.id}", {"type": "chat.message",
+                                                                                         "message": json.dumps(
+                                                                                             output_data_clients)})
+        else:
+            current_attachment.save()
+
+        responce_data = {
+            'event': "response_action",
+            'action': "upload",
+            'message': AttachmentSerializer(current_attachment).data,
+        }
+        self.send(text_data=json.dumps(responce_data))
 
 
     def receive(self, text_data):
