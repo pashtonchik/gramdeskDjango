f33edb5dc7ebf11b2651ed4ec65b5ba872abec4f
pashtonchik <pashka191@yandex.ru>
2023-10-03 00:56:12 +0300

    asdasdad

--- tickets/settings.py
+++ tickets/settings.py
@@ -157,38 +157,38 @@ with open('publicKey_jwt.pem', 'r') as content_file:
 
 
 
-# SIMPLE_JWT = {
-#     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-#     'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
-#     'ROTATE_REFRESH_TOKENS': True,
-#     'BLACKLIST_AFTER_ROTATION': True,
-#     'UPDATE_LAST_LOGIN': True,
-#
-#     'ALGORITHM': 'RS512',
-#     'SIGNING_KEY': private_key_jwt,
-#     'VERIFYING_KEY': pub_key_jwt,
-#     'AUDIENCE': None,
-#     'ISSUER': None,
-#     'JWK_URL': None,
-#     'LEEWAY': 0,
-#
-#     'AUTH_HEADER_TYPES': ('Bearer',),
-#     'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
-#     'USER_ID_FIELD': 'id',
-#     'USER_ID_CLAIM': 'user_id',
-#     'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
-#     "TOKEN_OBTAIN_SERIALIZER": "dispatcher.serializers.MyTokenObtainPairSerializer",
-#
-#     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-#     'TOKEN_TYPE_CLAIM': 'token_type',
-#     'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
-#     "TOKEN_REFRESH_SERIALIZER": "dispatcher.serializers.MyTokenRefreshSerializer",
-#     'JTI_CLAIM': 'jti',
-#
-#     'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-#     'SLIDING_TOKEN_LIFETIME': timedelta(days=10000),
-#     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=10000),
-# }
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': True,
+
+    'ALGORITHM': 'RS512',
+    'SIGNING_KEY': private_key_jwt,
+    'VERIFYING_KEY': pub_key_jwt,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    'AUTH_HEADER_TYPES': ('Token',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+    "TOKEN_OBTAIN_SERIALIZER": "dispatcher.serializers.MyTokenObtainPairSerializer",
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+    "TOKEN_REFRESH_SERIALIZER": "dispatcher.serializers.MyTokenRefreshSerializer",
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(days=10000),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=10000),
+}
 
 # PROFIAT_PUBKEY=123
 
