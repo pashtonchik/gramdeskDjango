5262b8650fffe5179aa92041c55ba7b18d42fade
pashtonchik <pashka191@yandex.ru>
2023-10-09 19:03:19 +0300

    asdasd

--- test.py
+++ test.py
@@ -20,30 +20,27 @@ def on_open(ws):
         time.sleep(2)
         received_size = 0
         len1 = 0
-        buf_size = 50000
-        full_size = os.path.getsize("WIN_20210621_12_33_56_Pro.jpg")
+        buf_size = 1000000
+        full_size = os.path.getsize("cmake-3.27.6-windows-x86_64.msi")
         print('full_size', full_size)
         time.sleep(1)
         data = ''
         while received_size < full_size:
             time.sleep(1)
 
-            with open('WIN_20210621_12_33_56_Pro.jpg', 'rb') as file:
-                print('весь', base64.b64encode(file.read()).decode('UTF-8'))
+            with open('cmake-3.27.6-windows-x86_64.msi', 'rb') as file:
+                print(1)
                 file.seek(0, 0)
-                print('весь в байтах', file.read(10))
                 # if received_size != 0:
                 file.seek(received_size, 0)
                 bytes = file.read(buf_size)
-                print('эти байты', bytes)
                 file = base64.b64encode(bytes).decode('UTF-8')
-                print(file)
             received_size += buf_size
             len1 += len(file)
             data += file
             # file = base64.b64encode(open('017-4852450_5920950975.pdf', 'r', encoding='utf-8').read()).decode('UTF-8')
             print(received_size)
-            ws.send(json.dumps({'event': "outgoing", 'action': "upload", 'upload_data': {"content": file, "id": "90"}}))
+            ws.send(json.dumps({'event': "outgoing", 'action': "upload", 'upload_data': {"content": file, "id": "433"}}))
 
         with open('127.pdf', 'ab+') as file:
             file.write(base64.b64decode(data))
@@ -56,8 +53,8 @@ def on_open(ws):
     _thread.start_new_thread(run, ())
 
 if __name__ == "__main__":
-    websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("wss://pashtonp.space/upload/",
+    websocket.enableTrace(False)
+    ws = websocket.WebSocketApp("wss://sptech409.space/upload/",
                               on_open=on_open,
                               on_message=on_message,
                               on_error=on_error,
