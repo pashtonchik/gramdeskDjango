a7ad13f09a5979a61b8d035fb8e621896906b371
pashtonchik <pashka191@yandex.ru>
2023-10-09 16:54:51 +0300

    asdasd

--- tickets/consumer.py
+++ tickets/consumer.py
@@ -126,7 +126,7 @@ class LiveScoreConsumer(WebsocketConsumer):
                         name=file['name'],
                         total_bytes=file['total_size'],
                         ext=file['ext'],
-                        buf_size=500000,
+                        buf_size=50000,
                     ).save()
             else:
                 message.save()
