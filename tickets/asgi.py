f90b1fadc0006f3a728a80128862469125d233d4
pashtonchik <pashka191@yandex.ru>
2023-10-09 21:49:34 +0300

    asdasdads

--- tickets/asgi.py
+++ tickets/asgi.py
@@ -1,27 +1,20 @@
-# import os
-#
-# import django
-# from channels.auth import AuthMiddlewareStack
-# from channels.security.websocket import AllowedHostsOriginValidator
-from django.conf import settings
-#
-# from django.core.asgi import get_asgi_application
-# from channels.routing import ProtocolTypeRouter, URLRouter, ChannelNameRouter
-# from django.urls import path, re_path
-#
-# from backend.consumer import LiveScoreConsumer
-# from backend.client_consumer import ClientConsumer
-# from backend.socket_auth import TokenAuthMiddleware
-# from backend.socket_heartbeat import HeartbeatMiddleware
-# from tickets.wsgi import *
-# from .wsgi import application
-settings.configure()
 import os
 
 import django
-from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.conf import settings
 
-from tickets.upload_cunsumer import UploadConsumer
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter, ChannelNameRouter
+from django.urls import path, re_path
+
+from backend.socket_auth import TokenAuthMiddleware
+from backend.socket_heartbeat import HeartbeatMiddleware
+from tickets.wsgi import *
+from .wsgi import application
+settings.configure()
+import os
 
 DJANGO_SETTINGS_MODULE = os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tickets.settings')
 django.setup()
@@ -34,6 +27,10 @@ from tickets.consumer import LiveScoreConsumer
 
 from backend.socket_auth import TokenAuthMiddleware
 from backend.socket_heartbeat import HeartbeatMiddleware
+import django
+from django.core.asgi import get_asgi_application
+
+from tickets.upload_cunsumer import UploadConsumer
 
 
 
