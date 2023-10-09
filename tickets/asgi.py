190735ce8c28b9fb9ad03b38a62dfa9a0983c271
pashtonchik <pashka191@yandex.ru>
2023-10-09 21:51:11 +0300

    asdasdads

--- tickets/asgi.py
+++ tickets/asgi.py
@@ -1,27 +1,17 @@
 import os
-
 import django
-from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.conf import settings
 
-settings.configure()
-import os
-
+# settings.configure()
 DJANGO_SETTINGS_MODULE = os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tickets.settings')
 django.setup()
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.urls import re_path
-
 from tickets.client_consumer import ClientConsumer
 from tickets.consumer import LiveScoreConsumer
-
 from backend.socket_auth import TokenAuthMiddleware
-from backend.socket_heartbeat import HeartbeatMiddleware
-import django
 from django.core.asgi import get_asgi_application
-
 from tickets.upload_cunsumer import UploadConsumer
 
 
