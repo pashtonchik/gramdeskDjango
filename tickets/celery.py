0fae4bdde0288363410035f4a0d21843818b5369
pashtonchik <pashka191@yandex.ru>
2024-03-15 12:43:29 +0300

    asdasda

--- tickets/celery.py
+++ tickets/celery.py
@@ -10,13 +10,3 @@ app = Celery(APP_NAME_CELERY)
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
-
-@app.on_after_configure.connect
-def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(15.0, update_items_fee.s())
-
-
-@app.task
-def update_items_fee():
-    heart_beat_connector()
-
