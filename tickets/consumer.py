32614066e7e367616ea442524776f87bc0aac2f1
pashtonchik <pashka191@yandex.ru>
2024-03-22 15:10:48 +0300

    asdasda

--- tickets/consumer.py
+++ tickets/consumer.py
@@ -89,9 +89,9 @@ class LiveScoreConsumer(WebsocketConsumer):
 
         ids_messages_to_output = message_to_output[:20].values_list('id', flat=True)
 
-        if message_to_output.filter(id__in=ids_messages_to_output, sender="client", sending_state="delivered").exists():
-            message_to_output.filter(id__in=ids_messages_to_output, sender="client", sending_state="delivered").update(sending_state="read")
-            send_message_read_messages.delay(ids_messages_to_output)
+        # if message_to_output.filter(id__in=ids_messages_to_output, sender="client", sending_state="delivered").exists():
+        message_to_output.filter(id__in=ids_messages_to_output, sender="client", sending_state="delivered").update(sending_state="read")
+        send_message_read_messages.delay(ids_messages_to_output)
 
         output_data = {}
         output_data['event'] = 'response_action'
