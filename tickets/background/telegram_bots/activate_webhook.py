8e9446b69ef69435d82978d8498ae0965bfc36ac
pashtonchik <pashka191@yandex.ru>
2024-03-22 15:39:18 +0300

    asdasda

--- tickets/background/telegram_bots/activate_webhook.py
+++ tickets/background/telegram_bots/activate_webhook.py
@@ -1,4 +1,6 @@
 import requests
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync, sync_to_async
 from celery import shared_task
 from django.db import transaction
 
@@ -24,4 +26,29 @@ def activate_webhook_telegram(bot_id):
             print("Какая то ошибка")
 
         bot.save()
+
+
+
+@shared_task()
+def send_message_read_messages(ids_array):
+    from backend.models import TicketMessage
+    from backend.serializers import TicketMessageSerializer
+    messages = TicketMessage.objects.filter(id__in=ids_array)
+
+    for message in messages:
+
+        data = {
+            'event': 'incoming',
+            'type': 'update_message',
+            'ok': True,
+            'message': TicketMessageSerializer(message, context={"from_user_type": message.sender}).data,
+        }
+
+        channel_layer = get_channel_layer()
+        if message.sender == "support":
+            async_to_sync(channel_layer.group_send)("active_support", {"type": "chat.message",
+                                                                   "message": json.dumps(data)})
+        else:
+            async_to_sync(channel_layer.group_send)(f"client_{message.tg_user.id}", {"type": "chat.message",
+                                                                                     "message": json.dumps(data)})
 
