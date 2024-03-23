6cde483d3b2460c37efce91e79e90eee5d370b88
pashtonchik <pashka191@yandex.ru>
2024-03-23 23:00:36 +0300

    asdasda

--- backend/serializers.py
+++ backend/serializers.py
@@ -97,7 +97,7 @@ class TicketSerializer(serializers.ModelSerializer):
         model = Ticket
 
     def get_last_message(self, obj):
-        return TicketMessageSerializer(obj.last_message, context=self.context).data
+        return TicketMessageSerializer(obj.last_message, context=self.context).data if obj.last_message else []
 
 
 class AttachmentSerializer(serializers.ModelSerializer):
