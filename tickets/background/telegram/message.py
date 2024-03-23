bf81656ffe0131928a992333024f2fedd64d20ea
pashtonchik <pashka191@yandex.ru>
2024-03-23 21:10:09 +0300

    asdasda

--- tickets/background/telegram/message.py
+++ tickets/background/telegram/message.py
@@ -10,27 +10,29 @@ def telegram_message(message_id):
     from backend.models import TicketMessage, TelegramBot
     from backend.serializers import TicketMessageSerializer
     from tickets.settings import SUPPORTBOT
+    from django.db import transaction
 
-    msg = TicketMessage.objects.get(id=message_id)
-    print('отправка сообщения в бот')
-    data = {
-        "chat_id": msg.tg_user.tg_id,
-        "parse_mode": "HTML",
-        "text": msg.message_text,
-    }
+    with transaction.atomic():
+        msg = TicketMessage.objects.select_for_update().get(id=message_id)
+        print('отправка сообщения в бот')
+        data = {
+            "chat_id": msg.tg_user.tg_id,
+            "parse_mode": "HTML",
+            "text": msg.message_text,
+        }
 
-    send_message = requests.get(
-        f"https://api.telegram.org/bot{TelegramBot.objects.get(platform=msg.ticket.platform).bot_apikey}/sendMessage", json=data)
-    print(send_message.status_code, send_message.text)
-    if send_message.status_code != 200:
         send_message = requests.get(
-            f"https://api.telegram.org/bot{SUPPORTBOT}/sendMessage", json=data)
+            f"https://api.telegram.org/bot{TelegramBot.objects.get(platform=msg.ticket.platform).bot_apikey}/sendMessage", json=data)
+        print(send_message.status_code, send_message.text)
+        if send_message.status_code != 200:
+            send_message = requests.get(
+                f"https://api.telegram.org/bot{SUPPORTBOT}/sendMessage", json=data)
 
-    if send_message.status_code == 200:
-        msg.sending_state = 'delivered'
-        msg.save()
+        if send_message.status_code == 200:
+            msg.sending_state = 'delivered'
+            msg.save()
 
-        channel_layer = get_channel_layer()
-        data = {'type': 'message_delivered', 'message': TicketMessageSerializer(msg).data}
-        async_to_sync(channel_layer.group_send)("active", {"type": "chat.message",
-                                                          "message": json.dumps(data)})
+            channel_layer = get_channel_layer()
+            data = {'type': 'message_delivered', 'message': TicketMessageSerializer(msg).data}
+            async_to_sync(channel_layer.group_send)("active", {"type": "chat.message",
+                                                              "message": json.dumps(data)})
