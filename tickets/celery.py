adeb33dbaecae92d946cda18e9fa80ab7b66415a
pashtonchik <pashka191@yandex.ru>
2024-03-29 15:09:57 +0300

    asdasda

--- tickets/celery.py
+++ tickets/celery.py
@@ -5,7 +5,7 @@ import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tickets.settings')
 
-app = Celery(APP_NAME_CELERY, backend=PORTREDIS)
+app = Celery(APP_NAME_CELERY, broker=PORTREDIS)
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
